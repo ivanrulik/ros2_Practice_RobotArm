2cf6b6d865dad3d9f2d7b0e1be3e869fd7e10e26
ivanrulik <ivanrulik@gmail.com>
2025-02-22 01:44:57 -0600

    description package added

--- src/practice_robotarm_py_examples/setup.py
+++ src/practice_robotarm_py_examples/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'simple_publisher = practice_robotarm_py_examples.simple_publisher:main',
+            'simple_subscriber = practice_robotarm_py_examples.simple_subscriber:main',
         ],
     },
 )
